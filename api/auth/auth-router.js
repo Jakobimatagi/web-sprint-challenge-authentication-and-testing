2dc0dce17eb0049427a4da7df35256c00fb841c1
Jakobi Matagi <jkmatagi@hotmail.com>
2021-05-15 10:01:37 -0600

    users

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,8 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs"); 
+const router = require("express").Router();
+const jwt = require('jsonwebtoken')
+const Users = require("../users/users-model.js"); 
+
 
 router.post('/register', (req, res) => {
   let creds = req.body;
@@ -19,37 +23,38 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  Users.findBy({ username })
-      .then(users => {
-          const user = users[0];
+    Users.findBy({ username })
+        .then(users => {
+            const user = users[0];
 
-          if (user && bcryptjs.compareSync(password, user.password)) {
-              const token = makeJwt(user)
+            if (user && bcryptjs.compareSync(password, user.password)) {
+                const token = makeJwt(user)
 
-              res.status(200).json({ message: "welcome!", token });
-          } else {
-              res.status(401).json({ message: "YOU SHALL NOT PASS" });
-          }
-        
-      })
-      .catch(error => {
-          res.status(500).json({ error: error.message });
-      });
+                res.status(200).json({ message: "welcome!", token });
+            } else {
+                res.status(401).json({ message: "YOU SHALL NOT PASS" });
+            }
+          
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+        });
 });
 
 function makeJwt(user){
-  const payload = {
-      subject: user.id,
-      username: user.username,
-      role: user.role
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        role: user.role
 }
 const secret = process.env.JWT_SECRET || 'is it secret'
 const options = {
-  expiresIn: '1h'
+    expiresIn: '1h'
 }
 return jwt.sign(payload, secret, options)
 }
+
 
 module.exports = router;
